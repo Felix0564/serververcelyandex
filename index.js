4e90ab61d90496ff51f5ef3cb124b2472afab0ec
Felix0564 <felixkouame6403@gmail.com>
2025-02-27 14:08:06 +0000

    first commit 3 pour vercel.json

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ io.on('connection', (socket) => {
 });
 
 // Démarrer le serveur
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
